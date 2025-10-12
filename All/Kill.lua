8e0cdf950256b2097df47eec1e17131a3a5d0881
Nuermek <nuermekgamer@gmail.com>
2025-10-12 19:51:32 +0700

    Delete All directory

--- All/Kill.lua
+++
@@ -1,9 +0,0 @@
-local npc = workspace:WaitForChild("Zombie")
-local humanoid = npc:WaitForChild("Humanoid")
-
-humanoid.Touched:Connect(function(hit)
-	local tool = hit.Parent
-	if tool and tool:FindFirstChild("IsWeapon") then
-		humanoid:TakeDamage(100)
-	end
-end)
